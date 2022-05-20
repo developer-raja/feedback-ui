60f23fd9ea003af502401bde2ab12ff4d921db49
raja <42689352+developer-raja@users.noreply.github.com>
2022-05-20 08:17:13 +0530

    components

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    rating: 7,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  }
+]
+
+export default FeedbackData;
